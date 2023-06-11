a227d4238d5df5dcca29b4e04f1c123ce3778c1c
Stef Robbe <stef.robbe@gmail.com>
2023-06-11 15:40:44 +0200

    completed retrieving recent vetoes from team

--- main.go
+++ main.go
@@ -8,19 +8,31 @@ import (
 
 // This exists to mimic how an end-user would use this module
 func main() {
-	// url := "https://www.hltv.org/stats/teams/maps/6667/faze?startDate=2023-02-28&endDate=2023-05-30&matchType=Lan&rankingFilter=Top20"
-	// url := "https://www.hltv.org/results?startDate=2023-03-10&endDate=2023-06-10&team=6667&matchType=Lan"
-	url := "https://www.hltv.org/matches/2364750/vitality-vs-faze-blast-premier-spring-final-2023"
+	// https://www.hltv.org/stats/teams/maps/6667/faze?startDate=2023-02-28&endDate=2023-05-30&matchType=Lan&rankingFilter=Top20
+	// https://www.hltv.org/results?startDate=2023-03-10&endDate=2023-06-10&team=6667&matchType=Lan
+	// https://www.hltv.org/matches/2364750/vitality-vs-faze-blast-premier-spring-final-2023
 
 	ctx, cancel := hltv.Init()
 	defer cancel()
 
-	// TODO: we have match links, now follow them and return vetoes...
-	stat, err := hltv.GetMatchVetoes(hltv.GetPage, ctx, url)
+	links, err := hltv.GetTeamRecentMatchLinks(hltv.GetPage, ctx, "https://www.hltv.org/results?startDate=2023-03-10&endDate=2023-06-10&team=6667&matchType=Lan", 3)
+
 	if err != nil {
 		msg := fmt.Sprintf("err retrieving stats: %v", err)
 		panic(msg)
 	}
+	fmt.Println("found these links:\n", links)
 
-	fmt.Println(stat)
+	var allVetoes []hltv.VetoStat
+	for _, l := range links {
+		vetoes, err := hltv.GetMatchVetoes(hltv.GetPage, ctx, l)
+		allVetoes = append(allVetoes, vetoes...)
+
+		if err != nil {
+			msg := fmt.Sprintf("err retrieving stats: %v", err)
+			panic(msg)
+		}
+	}
+
+	fmt.Println("And here are the vetoes from FaZe:\n", allVetoes)
 }

878080a5e5a99d091f5211069bc1fbcb271b5e65
Stef Robbe <stef.robbe@gmail.com>
2023-06-11 15:40:28 +0200

    fixed last veto parsing

--- pkg/hltv.go
+++ pkg/hltv.go
@@ -75,28 +75,52 @@ func GetTeamRecentMatchLinks(getPage func(context.Context, *string, string) erro
 		return nil, err
 	}
 
-	doc.Find("a.a-reset").Each(func(_ int, sel *goquery.Selection) {
+	linkCount := 0
+	doc.Find("div.allres a.a-reset").EachWithBreak(func(_ int, sel *goquery.Selection) bool {
+		if linkCount == limit {
+			return false
+		}
 		ml, ok := sel.Attr("href")
 		if ok && strings.Contains(ml, "matches") {
-			matchLinks = append(matchLinks, ml)
+			fmt.Println(ml)
+			correctUrl := "https://www.hltv.org" + ml
+			matchLinks = append(matchLinks, correctUrl)
+
+			linkCount++
 		}
+
+		return true
 	})
 
 	return matchLinks, nil
 }
 
-// TODO: I want this in internal, how to without cyclical import?
-func parseVeto(strVeto string) VetoStat {
+// ? TODO: I want this in internal, how to without cyclical import?
+func parseVeto(strVeto string, lastTeam string) (VetoStat, string) {
 	vs := VetoStat{}
 	re := regexp.MustCompile(`\S+`)
 
 	submatchall := re.FindAllString(strVeto, -1)
+
+	// * here we handle the last veto: in HLTV it will say "map X was left over",
+	// * which effectively means that the team that vetoed last
+	// * "picked" the map that was left over and banned the previous one...
+	// ? this means we have an extra pick/ban, is this the way to go?
+	// TODO: this will probably not work with auto-banned maps, where a team got a one-map advantage from an upper-bracket run for example...
+	if strings.Contains(strVeto, "was left over") {
+		vs.Team = lastTeam
+		vs.PickBan = Picked
+		vs.Map, _ = MapString(submatchall[1])
+
+		return vs, lastTeam
+	}
+
 	// TODO: handle error cases
-	// TODO: handle last veto: in HLTV it will say "map X was left over", which effectively means that the team that vetoed last picked the map that was left over...
 	vs.Team = submatchall[1]
+	lastTeam = vs.Team
 	vs.PickBan, _ = VetoString(submatchall[2])
 	vs.Map, _ = MapString(submatchall[3])
-	return vs
+	return vs, lastTeam
 }
 
 func GetMatchVetoes(getPage func(context.Context, *string, string) error, ctx context.Context, url string) ([]VetoStat, error) {
@@ -114,14 +138,20 @@ func GetMatchVetoes(getPage func(context.Context, *string, string) error, ctx co
 
 	// not sure why but 3rd child works...
 	doc.Find("div.standard-box.veto-box:nth-child(3) div.padding").Each(func(_ int, sel *goquery.Selection) {
-		// HLTV has shitty HTML in this section, all of the map vetoes
-		// are seen as a single string, so we have to split on "\n" and remove
-		// any empty lines:
+		// * HLTV has shitty HTML in this section, all of the map vetoes
+		// * are seen as a single string, so we have to split on "\n" and remove
+		// * any empty lines:
+
+		var lastTeam string
+		var vs VetoStat
+
 		vetoes := strings.Split(sel.Text(), "\n")
+
 		for _, v := range vetoes {
 			tmp := strings.Join(strings.Fields(v), " ")
 			if tmp != "" {
-				matchVetoes = append(matchVetoes, parseVeto(tmp))
+				vs, lastTeam = parseVeto(tmp, lastTeam)
+				matchVetoes = append(matchVetoes, vs)
 			}
 		}
 	})
